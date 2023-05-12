5dcb0d21ee651cde59bc2f4afb136104c1453145
jmd12a <928814697@qq.com>
2023-05-12 22:16:47 +0800

    项目初步构建

---
+++ gulimall-coupon/src/test/java/com/keda/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keda.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-goods/src/test/java/com/keda/gulimall/goods/GulimallGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keda.gulimall.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
